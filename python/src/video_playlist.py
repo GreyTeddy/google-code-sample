1220504a1e0fe1b5bb4a36f3876557e80859e819
Dennis Duka <dukadennis7@gmail.com>
2021-06-30 15:33:35 +0100

    implemented Playlist class

--- python/src/video_playlist.py
+++ python/src/video_playlist.py
@@ -3,3 +3,10 @@
 
 class Playlist:
     """A class used to represent a Playlist."""
+
+    """
+        Args:
+            name: The playlist's name.
+    """
+    def __init__(self,playlist_name):
+        self._playlist_name = playlist_name
